7e0382d5d083ce9e6ebbeee118797d65695b636e
Vanderlin <todd@vanderlin.cc>
2023-10-12 11:54:41 -0400

    paths

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hi Climate');
-});
+app.use('/', express.static(path.join(__dirname, 'unity')));
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
